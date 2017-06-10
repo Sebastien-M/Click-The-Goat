e55f62f512872543a8adf890bbe85c3be7e24a21
Sébastien Mandaba <seb.mandaba@gmail.com>
2017-06-10 18:44:08 +0200

    save stuff to add

--- game.php
+++ game.php
@@ -4,14 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css?family=Patrick+Hand+SC" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/png" href="icon/goat.png" />
+    <title>Click the goat!</title>
 </head>
 <body>
-    <main>
-        <div class="img">
-            <!--<img src="goat.jpg" alt="">-->
-            <p><?php echo $_POST['name']; ?></p>
-        </div>
+    
+        <?php
+            session_start();
+            $_SESSION['name'] = $_POST['name'];
+        ?>
+        <header>
+            <div class="titre">
+                <h1>Click the goat!</h1>
+            </div>
+            <section class="infos">
+                <p class="eh"><?php echo $_POST['name']; ?></p>
+                <p class="eh">score : </p>
+                <p id="score">0</p>
+                <input type="submit"  class="eh" value="save">
+            </section>
+        </header>
+        <main>
+        <span class="img">
+            <img id="goat" src="img/goat02.gif" alt="goat" width="500" height="400">
+        </span>
     </main>
+    <script src="js/main.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -6,55 +6,63 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css?family=Patrick+Hand+SC" rel="stylesheet">
+    <link rel="icon" type="image/png" href="icon/goat.png" />
     <title>Click me!</title>
     <style>
         html {
             font-family: 'Patrick Hand SC', cursive;
         }
-
+        
         h1 {
             text-align: center;
             padding-top: 20vh;
             font-size: 60px;
             color: white;
         }
-
+        
         form {
             display: flex;
             flex-direction: column;
             align-items: center;
         }
-
+        
         #name {
             text-align: center;
             width: 100%;
             border: none;
-            height:30px;
-            transition:0.5s;
+            height: 40px;
+            transition: 0.5s;
         }
-
+        
         #sub {
+            width: 15%;
             border: none;
-            background-color:white;
-            color:black;
-            transition:0.5s;
+            background-color: white;
+            color: black;
+            transition: 0.5s;
         }
-        #sub:hover{
-            background-color:black;
-            color:white;
+        
+        #sub:hover {
+            background-color: black;
+            color: white;
         }
-
+        
         body {
-            background: linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('goat_bg.jpg');
+            background: linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('img/goat_bg.jpg');
         }
-        .in{
-            width:40%;
-            display:flex;
+        
+        .in {
+            width: 40%;
+            display: flex;
         }
     </style>
 </head>
 
 <body>
+    <?php
+    session_start();
+    session_destroy();
+     ?>
     <h1>Click the goat!</h1>
     <form action="game.php" method="POST">
         <div class="in">
@@ -62,7 +70,6 @@
             <input type="submit" value="Submit" id="sub">
         </div>
     </form>
-    <script src="js/main.js"></script>
 </body>
 
 </html>
